7fbd9a06f9bf3f666af15d592b13b01ce212c4b0
intensivee <mrorange016@gmail.com>
2021-03-07 14:11:30 +0100

    Tests for service layer provided;

---
+++ src/test/java/com/remi/calculator/service/NbpApiClientTest.java
@@ -0,0 +1,38 @@
+package com.remi.calculator.service;
+
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class NbpApiClientTest {
+
+    private final NbpApiClient nbpApiClient;
+
+    @Autowired
+    public NbpApiClientTest(NbpApiClient nbpApiClient) {
+        this.nbpApiClient = nbpApiClient;
+    }
+
+    @Test
+    void givenWrongCurrencyCode_throwsException() {
+        String wrongCurrencyCode = "Some Wrong Code";
+
+        assertThrows(FeignException.class, () -> {
+           this.nbpApiClient.getCurrencyStatus(wrongCurrencyCode);
+        });
+    }
+
+    @Test
+    void givenGbpCurrencyCode_returnsGbpValue() {
+        String expectedCurrencyCode = "GBP";
+
+        String actualCurrencyCode = this.nbpApiClient.getCurrencyStatus(expectedCurrencyCode).getCode();
+
+        assertEquals(expectedCurrencyCode, actualCurrencyCode);
+    }
+}
---
+++ src/test/java/com/remi/calculator/service/RateServiceTest.java
@@ -0,0 +1,38 @@
+package com.remi.calculator.service;
+
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class RateServiceTest {
+    
+    private final RateService rateService;
+
+    @Autowired
+    public RateServiceTest(RateService rateService) {
+        this.rateService = rateService;
+    }
+
+    @Test
+    void givenWrongCurrencyCode_throwsException() {
+        String wrongCurrencyCode = "Some Wrong Code";
+
+        assertThrows(FeignException.class, () -> {
+            this.rateService.getCurrentRateByCurrencyCode(wrongCurrencyCode);
+        });
+    }
+
+    @Test
+    void givenGbpCurrencyCode_returnsPositiveRate() {
+        String currencyCode = "GBP";
+
+        Double rate = this.rateService.getCurrentRateByCurrencyCode(currencyCode);
+
+        assertTrue(rate > 0);
+    }
+}
